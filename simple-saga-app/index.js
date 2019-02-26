fe233d9c19eb3cbfd085c2554a5c925593b4442d
damarendra <damarendran@gmail.com>
2019-02-26 12:20:24 +0700

    Added eslint and editorconfig

---
+++ simple-saga-app/index.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import {
+    View,
+    Text
+} from 'react-native';
+
+export const component = () => {
+    return(
+        <View>
+            <Text>
+                Hello
+            </Text>
+        </View>
+    );
+};
